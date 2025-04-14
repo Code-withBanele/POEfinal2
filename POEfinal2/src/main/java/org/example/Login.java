a7a9e0c37850e10a6e8c396b8884efa956ce8a38
Code-withBanele <ST10489580@rcconnect.edu.za>
2025-04-14 23:12:57 +0200

    Update Login.java

--- POEfinal2/src/main/java/org/example/Login.java
+++ POEfinal2/src/main/java/org/example/Login.java
@@ -1,12 +1,20 @@
+//importing package from org.example
+
 package org.example;
+
+//declaring our login class
 class Login {
+
+    //creating a registration object inside the class
     private Registration registration;
 
+    //login class with the registration objectt passed as a parameter to fetch the user details
     public Login(Registration registration)
     {
         this.registration = registration;
     }
 
+    //boolean method that checks if the enteterd credenttials match tthe credentials from the records.   
     public boolean authenticate(String userName, String password)
 
     {
